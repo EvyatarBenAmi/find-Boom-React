c0b3f7bbca01d9d122f850657c548bb3b37a6fc5
Pinchas Lifshitz <pnl8480537@gmail.com>
2026-02-24 15:59:11 +0200

    added bomds.js file

--- src/Network.jsx
+++ src/Network.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
-
+import React, { useRef, useState } from 'react'
+import {bomds} from "./bombs"
 export default function Network() {
     const [network, setNetwork] = useState([])
     for (let i = 1; i <= 100; i++) {
         network.push(i)
     }
+    const boms = useRef(bomds())
+    
     return (
         <div className='main'>
             <div className='network'>
---
+++ src/bombs.js
@@ -0,0 +1,8 @@
+export function bomds(){
+    const bon1 = (Math.floor(Math.random()*100)+1)
+    const bon2 = (Math.floor(Math.random()*100)+1)
+    const bon3 = (Math.floor(Math.random()*100)+1)
+    const bon4 = (Math.floor(Math.random()*100)+1)
+    const bon5 = (Math.floor(Math.random()*100)+1)
+    return[bon1 ,bon2,bon3,bon4,bon5]
+}
